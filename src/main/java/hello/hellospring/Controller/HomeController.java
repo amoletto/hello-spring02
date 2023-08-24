7faad23d0475a4e0a3a9adb6fc96cac7b54d8ce5
amoletto <bb00q@naver.com>
2023-08-24 17:11:03 +0900

    develop_230824

--- src/main/java/hello/hellospring/Controller/HomeController.java
+++ src/main/java/hello/hellospring/Controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    //testgdfgdfgdfgd
     @GetMapping("/")
     public String home() {
         return "home";
